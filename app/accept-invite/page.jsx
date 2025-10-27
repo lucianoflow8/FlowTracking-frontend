86e46c1c7965331b2e8fab5dfd43271485980ded
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-10-27 20:59:08 -0300

    fix: remove accept-invite routes to resolve prerender errors

--- app/accept-invite/page.jsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-
-import { redirect } from "next/navigation";
-
-export default function Page({ searchParams }) {
-  const qs = new URLSearchParams(searchParams).toString();
-  redirect(`/accept-invite${qs ? `?${qs}` : ""}`);
-}
