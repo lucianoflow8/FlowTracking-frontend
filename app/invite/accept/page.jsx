5d16194d24e1ca1d16346f79980d9bd63aa63dae
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-10-27 17:47:28 -0300

    fix: disable prerender for dynamic pages

--- app/invite/accept/page.jsx
+++ app/invite/accept/page.jsx
@@ -1,4 +1,7 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
