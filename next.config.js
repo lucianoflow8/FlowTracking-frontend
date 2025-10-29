8abf8227850107ca8d322d5816065805abd914fc
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-10-28 21:46:44 -0300

    fix: next.config.js experimental.serverActions object

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: "standalone",
 
   experimental: {
-    serverActions: false,
+    serverActions: {}, // ✅ debe ser objeto, no boolean
   },
 
   // ✅ Forzamos que TODAS las páginas se sirvan dinámicamente
